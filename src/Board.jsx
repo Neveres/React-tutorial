bccf7667b55828480cb6547b917d3088e1daf48d
LeoWu <lechewu@gmail.com>
2019-04-16 15:09:31 +0800

    [refactor] Remove unused script

--- src/Board.jsx
+++ src/Board.jsx
@@ -37,21 +37,6 @@ const Board = (props) => {
     return (
         <div>
             {renderSquares(squares)}
-            {/* <div className="board-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
-            </div>
-            <div className="board-row">
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
-            </div> */}
         </div>
     );
 };
